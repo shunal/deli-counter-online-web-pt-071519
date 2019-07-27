9316833603e78877d1c9616f21f04a9e9bb85f34
Steven Hunal <stevehunal@gmail.com>
2019-07-27 23:01:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 # Write your code here.
 def line(array)
+<<<<<<< HEAD
 if array.count == 0
   puts "The line is currently empty."
 else 
@@ -25,5 +26,17 @@ puts "Currently serving #{first_person}."
   array.shift
   else
     puts "There is nobody waiting to be served!"
+=======
+new_array = []
+if array.count == 0
+  puts "The line is currently empty."
+else 
+  string = "The line is currently: "
+  array.each.with_index(1) do |name, index|
+  current_line = string + name
+  
+    end
+    puts current_line
+>>>>>>> c44a0a7dc5f8562a75167718961d7d1f14107e02
   end
 end
